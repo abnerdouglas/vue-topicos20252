2b743dc98900b525fecc4a1be5dd16cb52db8232
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-11-04 10:52:45 -0300

    Tela de anotações

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,30 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import { usuarioStore } from './stores/usuario'
 
 axios.defaults.baseURL = 'https://super-duper-waddle-q5qjqq5vj5h4qjr-8080.app.github.dev/'
 
+axios.interceptors.request.use((config) => {
+  const store = usuarioStore()
+  if (store.token) {
+    config.headers.Authorization = store.token
+  }
+  return config
+})
+
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+
 app.use(router)
 
 app.mount('#app')
